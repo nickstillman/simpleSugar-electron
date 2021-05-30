540acd04ff3e93366d6ae7e6bdb0144ee9172c02
Nick Stillman <nick.edward.stillman@gmail.com>
2021-05-30 12:34:02 -0400

    basic electron setup with text area & links working

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,157 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Link, HashRouter as Router, Switch, Route } from 'react-router-dom';
 import icon from '../assets/icon.svg';
 import './App.global.css';
+
+const { useState, useEffect } = React;
+
+// const Hello = () => {
+//   return (
+//     <div>
+//     <div className="Hello">
+//     <img width="200px" alt="icon" src={icon} />
+//     </div>
+//     <h1>hiya from simpleSugar</h1>
+//     <div className="Hello">
+//     <Link to="/home">
+//     <button type="button">
+//     <span role="img" aria-label="books">
+//     📚
+//     </span>
+//     HOME
+//     </button>
+//     </Link>
+//     <a
+//     href="https://electron-react-boilerplate.js.org/"
+//     target="_blank"
+//     rel="noreferrer"
+//     >
+//     <button type="button">
+//     <span role="img" aria-label="books">
+//     📚
+//     </span>
+//     Read our docs
+//     </button>
+//     </a>
+//     <a
+//     href="https://github.com/sponsors/electron-react-boilerplate"
+//     target="_blank"
+//     rel="noreferrer"
+//     >
+//     <button type="button">
+//     <span role="img" aria-label="books">
+//     🙏
+//     </span>
+//     Donate
+//     </button>
+//     </a>
+//     </div>
+//     </div>
+//     );
+//   };
 
 const Hello = () => {
   return (
     <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
+    <div className="basic-centered">
+    <img width="200px" alt="icon" src={icon} />
     </div>
+    <h1 className="basic-centered">simpleSugar</h1>
+    <h2 className="basic-centered">Diabetes Tech for Diabetics Who Don't Use Tech</h2>
+    <div className="basic-centered">
+    <Link to="/home">
+    <button type="button">
+    <span role="img" aria-label="books">
+    📚
+    </span>
+    HOME
+    </button>
+    </Link>
+    </div>
+    </div>
+    );
+  };
+
+
+  const Home = () => {
+    const [ text, setText ] = useState('');
+    const [ currentInput, setCurrentInput ] = useState('');
+
+    const handleInput = (e: any) => {
+      const val = e.target.value;
+      const char = val.charCodeAt(val.length - 1)
+      setText(val);
+      console.log('TYPED:', val);
+      console.log('text:', text);
+      // console.log('endChar', char);
+      if (char === 10) {
+        setCurrentInput(val);
+        setText('');
+        console.log('process:', val, 'input:', currentInput);
+      }
+    }
+
+    useEffect(() => {
+      console.log('textEFFECT:', text);
+      console.log('currentInputEFFECT:', currentInput);
+    })
+
+    return (
+      <div>
+      <div className="basic-centered">
+      <h1>Hi from simpleSugar!</h1>
+      </div>
+      <div>
+      <Link className="basic-centered" to="/">
+      <button type="button">
+      <span role="img" aria-label="books">
+      📚
+      </span>
+      Back to Title Page
+      </button>
+      </Link>
+      </div>
+
+      {/* <form className="inputArea">
+      <label>
+      <textarea autoComplete="off" autoFocus className="textArea" value={ text } onChange={ setText( text ) } />
+      </label>
+    </form> */}
+
+    <form className="inputArea">
+    <label>
+    <textarea autoComplete="off" autoFocus className="textArea" value={ text } onChange={ (e) => {
+      handleInput(e);
+    }
+  } />
+  </label>
+  </form>
+  <div>
+  <div className="basic-centered">
+  <h1>NOW:
+  { currentInput }
+  </h1>
+  </div>
+  </div>
+  {/* <p>
+    { text }
+    </p>
+
+  <button onClick={() => setText(text + '!')}></button> */}
+
+
+  </div>
   );
 };
 
 export default function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" component={Hello} />
-      </Switch>
+    <Switch>
+    <Route exact path="/" component={Hello} />
+    <Route path="/home" component={Home} />
+    </Switch>
     </Router>
-  );
-}
+    );
+  }
--- src/menu.ts
+++ src/menu.ts
@@ -22,149 +22,158 @@ export default class MenuBuilder {
     if (
       process.env.NODE_ENV === 'development' ||
       process.env.DEBUG_PROD === 'true'
-    ) {
-      this.setupDevelopmentEnvironment();
+      ) {
+        this.setupDevelopmentEnvironment();
+      }
+
+      const template =
+      process.platform === 'darwin'
+      ? this.buildDarwinTemplate()
+      : this.buildDefaultTemplate();
+
+      const menu = Menu.buildFromTemplate(template);
+      Menu.setApplicationMenu(menu);
+
+      return menu;
     }
 
-    const template =
-      process.platform === 'darwin'
-        ? this.buildDarwinTemplate()
-        : this.buildDefaultTemplate();
+    setupDevelopmentEnvironment(): void {
+      this.mainWindow.webContents.on('context-menu', (_, props) => {
+        const { x, y } = props;
+        Menu.buildFromTemplate([
+          {
+            label: 'Inspect element',
+            click: () => {
+              this.mainWindow.webContents.inspectElement(x, y);
+            },
+          },
+        ]).popup({ window: this.mainWindow });
+      });
+    }
 
-    const menu = Menu.buildFromTemplate(template);
-    Menu.setApplicationMenu(menu);
-
-    return menu;
-  }
-
-  setupDevelopmentEnvironment(): void {
-    this.mainWindow.webContents.on('context-menu', (_, props) => {
-      const { x, y } = props;
-
-      Menu.buildFromTemplate([
-        {
-          label: 'Inspect element',
-          click: () => {
-            this.mainWindow.webContents.inspectElement(x, y);
+    buildDarwinTemplate(): MenuItemConstructorOptions[] {
+      const subMenuAbout: DarwinMenuItemConstructorOptions = {
+        label: 'simpleSugar!',
+        submenu: [
+          {
+            label: 'About simpleSugar',
+            selector: 'orderFrontStandardAboutPanel:',
           },
-        },
-      ]).popup({ window: this.mainWindow });
-    });
-  }
-
-  buildDarwinTemplate(): MenuItemConstructorOptions[] {
-    const subMenuAbout: DarwinMenuItemConstructorOptions = {
-      label: 'Electron',
-      submenu: [
-        {
-          label: 'About ElectronReact',
-          selector: 'orderFrontStandardAboutPanel:',
-        },
-        { type: 'separator' },
-        { label: 'Services', submenu: [] },
-        { type: 'separator' },
-        {
-          label: 'Hide ElectronReact',
-          accelerator: 'Command+H',
-          selector: 'hide:',
-        },
-        {
-          label: 'Hide Others',
-          accelerator: 'Command+Shift+H',
-          selector: 'hideOtherApplications:',
-        },
-        { label: 'Show All', selector: 'unhideAllApplications:' },
-        { type: 'separator' },
-        {
-          label: 'Quit',
-          accelerator: 'Command+Q',
-          click: () => {
-            app.quit();
+          { type: 'separator' },
+          { label: 'Services', submenu: [] },
+          { type: 'separator' },
+          {
+            label: 'Hide simpleSugar',
+            accelerator: 'Command+H',
+            selector: 'hide:',
           },
-        },
-      ],
-    };
-    const subMenuEdit: DarwinMenuItemConstructorOptions = {
-      label: 'Edit',
-      submenu: [
-        { label: 'Undo', accelerator: 'Command+Z', selector: 'undo:' },
-        { label: 'Redo', accelerator: 'Shift+Command+Z', selector: 'redo:' },
-        { type: 'separator' },
-        { label: 'Cut', accelerator: 'Command+X', selector: 'cut:' },
-        { label: 'Copy', accelerator: 'Command+C', selector: 'copy:' },
-        { label: 'Paste', accelerator: 'Command+V', selector: 'paste:' },
-        {
-          label: 'Select All',
-          accelerator: 'Command+A',
-          selector: 'selectAll:',
-        },
-      ],
-    };
-    const subMenuViewDev: MenuItemConstructorOptions = {
-      label: 'View',
-      submenu: [
-        {
-          label: 'Reload',
-          accelerator: 'Command+R',
-          click: () => {
-            this.mainWindow.webContents.reload();
+          {
+            label: 'Hide Others',
+            accelerator: 'Command+Shift+H',
+            selector: 'hideOtherApplications:',
           },
-        },
-        {
-          label: 'Toggle Full Screen',
-          accelerator: 'Ctrl+Command+F',
-          click: () => {
-            this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
+          { label: 'Show All', selector: 'unhideAllApplications:' },
+          { type: 'separator' },
+          {
+            label: 'Quit',
+            accelerator: 'Command+Q',
+            click: () => {
+              app.quit();
+            },
           },
-        },
-        {
-          label: 'Toggle Developer Tools',
-          accelerator: 'Alt+Command+I',
-          click: () => {
-            this.mainWindow.webContents.toggleDevTools();
+        ],
+      };
+      const subMenuEdit: DarwinMenuItemConstructorOptions = {
+        label: 'Edit',
+        submenu: [
+          { label: 'Undo', accelerator: 'Command+Z', selector: 'undo:' },
+          { label: 'Redo', accelerator: 'Shift+Command+Z', selector: 'redo:' },
+          { type: 'separator' },
+          { label: 'Cut', accelerator: 'Command+X', selector: 'cut:' },
+          { label: 'Copy', accelerator: 'Command+C', selector: 'copy:' },
+          { label: 'Paste', accelerator: 'Command+V', selector: 'paste:' },
+          {
+            label: 'Select All',
+            accelerator: 'Command+A',
+            selector: 'selectAll:',
           },
-        },
-      ],
-    };
-    const subMenuViewProd: MenuItemConstructorOptions = {
-      label: 'View',
-      submenu: [
-        {
-          label: 'Toggle Full Screen',
-          accelerator: 'Ctrl+Command+F',
-          click: () => {
-            this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
+          { label: 'Export',
+          submenu: [
+            {label: 'All',
+            accelerator: 'Command+E',
+            click: () => {
+              console.log('Export all!!!');
+            },
           },
+        ],
+      },
+    ],
+  };
+  const subMenuViewDev: MenuItemConstructorOptions = {
+    label: 'View',
+    submenu: [
+      {
+        label: 'Reload',
+        accelerator: 'Command+R',
+        click: () => {
+          this.mainWindow.webContents.reload();
         },
-      ],
-    };
-    const subMenuWindow: DarwinMenuItemConstructorOptions = {
-      label: 'Window',
-      submenu: [
-        {
-          label: 'Minimize',
-          accelerator: 'Command+M',
-          selector: 'performMiniaturize:',
+      },
+      {
+        label: 'Toggle Full Screen',
+        accelerator: 'Ctrl+Command+F',
+        click: () => {
+          this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
         },
-        { label: 'Close', accelerator: 'Command+W', selector: 'performClose:' },
-        { type: 'separator' },
-        { label: 'Bring All to Front', selector: 'arrangeInFront:' },
-      ],
-    };
-    const subMenuHelp: MenuItemConstructorOptions = {
-      label: 'Help',
-      submenu: [
-        {
-          label: 'Learn More',
-          click() {
-            shell.openExternal('https://electronjs.org');
-          },
+      },
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: 'Alt+Command+I',
+        click: () => {
+          this.mainWindow.webContents.toggleDevTools();
         },
-        {
-          label: 'Documentation',
-          click() {
-            shell.openExternal(
-              'https://github.com/electron/electron/tree/master/docs#readme'
+      },
+    ],
+  };
+  const subMenuViewProd: MenuItemConstructorOptions = {
+    label: 'View',
+    submenu: [
+      {
+        label: 'Toggle Full Screen',
+        accelerator: 'Ctrl+Command+F',
+        click: () => {
+          this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
+        },
+      },
+    ],
+  };
+  const subMenuWindow: DarwinMenuItemConstructorOptions = {
+    label: 'Window',
+    submenu: [
+      {
+        label: 'Minimize',
+        accelerator: 'Command+M',
+        selector: 'performMiniaturize:',
+      },
+      { label: 'Close', accelerator: 'Command+W', selector: 'performClose:' },
+      { type: 'separator' },
+      { label: 'Bring All to Front', selector: 'arrangeInFront:' },
+    ],
+  };
+  const subMenuHelp: MenuItemConstructorOptions = {
+    label: 'Help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click() {
+          shell.openExternal('https://electronjs.org');
+        },
+      },
+      {
+        label: 'Documentation',
+        click() {
+          shell.openExternal(
+            'https://github.com/electron/electron/tree/master/docs#readme'
             );
           },
         },
@@ -184,10 +193,10 @@ export default class MenuBuilder {
     };
 
     const subMenuView =
-      process.env.NODE_ENV === 'development' ||
-      process.env.DEBUG_PROD === 'true'
-        ? subMenuViewDev
-        : subMenuViewProd;
+    process.env.NODE_ENV === 'development' ||
+    process.env.DEBUG_PROD === 'true'
+    ? subMenuViewDev
+    : subMenuViewProd;
 
     return [subMenuAbout, subMenuEdit, subMenuView, subMenuWindow, subMenuHelp];
   }
@@ -213,78 +222,78 @@ export default class MenuBuilder {
       {
         label: '&View',
         submenu:
-          process.env.NODE_ENV === 'development' ||
-          process.env.DEBUG_PROD === 'true'
-            ? [
-                {
-                  label: '&Reload',
-                  accelerator: 'Ctrl+R',
-                  click: () => {
-                    this.mainWindow.webContents.reload();
-                  },
+        process.env.NODE_ENV === 'development' ||
+        process.env.DEBUG_PROD === 'true'
+        ? [
+          {
+            label: '&Reload',
+            accelerator: 'Ctrl+R',
+            click: () => {
+              this.mainWindow.webContents.reload();
+            },
+          },
+          {
+            label: 'Toggle &Full Screen',
+            accelerator: 'F11',
+            click: () => {
+              this.mainWindow.setFullScreen(
+                !this.mainWindow.isFullScreen()
+                );
+              },
+            },
+            {
+              label: 'Toggle &Developer Tools',
+              accelerator: 'Alt+Ctrl+I',
+              click: () => {
+                this.mainWindow.webContents.toggleDevTools();
+              },
+            },
+          ]
+          : [
+            {
+              label: 'Toggle &Full Screen',
+              accelerator: 'F11',
+              click: () => {
+                this.mainWindow.setFullScreen(
+                  !this.mainWindow.isFullScreen()
+                  );
                 },
-                {
-                  label: 'Toggle &Full Screen',
-                  accelerator: 'F11',
-                  click: () => {
-                    this.mainWindow.setFullScreen(
-                      !this.mainWindow.isFullScreen()
+              },
+            ],
+          },
+          {
+            label: 'Help',
+            submenu: [
+              {
+                label: 'Learn More',
+                click() {
+                  shell.openExternal('https://electronjs.org');
+                },
+              },
+              {
+                label: 'Documentation',
+                click() {
+                  shell.openExternal(
+                    'https://github.com/electron/electron/tree/master/docs#readme'
                     );
                   },
                 },
                 {
-                  label: 'Toggle &Developer Tools',
-                  accelerator: 'Alt+Ctrl+I',
-                  click: () => {
-                    this.mainWindow.webContents.toggleDevTools();
+                  label: 'Community Discussions',
+                  click() {
+                    shell.openExternal('https://www.electronjs.org/community');
                   },
                 },
-              ]
-            : [
                 {
-                  label: 'Toggle &Full Screen',
-                  accelerator: 'F11',
-                  click: () => {
-                    this.mainWindow.setFullScreen(
-                      !this.mainWindow.isFullScreen()
-                    );
+                  label: 'Search Issues',
+                  click() {
+                    shell.openExternal('https://github.com/electron/electron/issues');
                   },
                 },
               ],
-      },
-      {
-        label: 'Help',
-        submenu: [
-          {
-            label: 'Learn More',
-            click() {
-              shell.openExternal('https://electronjs.org');
             },
-          },
-          {
-            label: 'Documentation',
-            click() {
-              shell.openExternal(
-                'https://github.com/electron/electron/tree/master/docs#readme'
-              );
-            },
-          },
-          {
-            label: 'Community Discussions',
-            click() {
-              shell.openExternal('https://www.electronjs.org/community');
-            },
-          },
-          {
-            label: 'Search Issues',
-            click() {
-              shell.openExternal('https://github.com/electron/electron/issues');
-            },
-          },
-        ],
-      },
-    ];
+          ];
 
-    return templateDefault;
-  }
-}
+          return templateDefault;
+        }
+      }
